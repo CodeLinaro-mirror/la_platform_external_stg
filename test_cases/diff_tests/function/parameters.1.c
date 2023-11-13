a481f61bd83fd594c4274bc6a106e7f6a5aa04a9
Giuliano Procida <gprocida@google.com>
2023-11-13 18:07:50 +0000

    test cases: adjust function/parameters tests to work with gcc -O2
    These cases are tweaked to avoid triggering a GCC bug where optimisation
    destroys some debug information.
    
    It suffices to ensure that all the functions compile to different code.
    
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=112372
    
    PiperOrigin-RevId: 582002413
    Change-Id: Ib5a1ba9f3d6e12c380e67a4b8d4968ee4dd7956b

--- test_cases/diff_tests/function/parameters.1.c
+++ test_cases/diff_tests/function/parameters.1.c
@@ -1,15 +1,15 @@
-int f01(int b, int c) { return b + c; }
-int f02(int a, int c) { return a + c; }
-int f03(int a, int b) { return a + b; }
-int f04(int b, int a, int c) { return a + b + c; }
-int f05(int a, int c, int b) { return a + b + c; }
-int f06(int c, int b, int a) { return a + b + c; }
-int f07(int b, int c, int a) { return a + b + c; }
-int f08(int c, int a, int b) { return a + b + c; }
-int f09(int d, int a, int b, int c) { return a + b + c + d; }
-int f10(int a, int d, int b, int c) { return a + b + c + d; }
-int f11(int a, int b, int d, int c) { return a + b + c + d; }
-int f12(int a, int b, int c, int d) { return a + b + c + d; }
+int f01(int b, int c) { return b + c + 1; }
+int f02(int a, int c) { return a + c + 2; }
+int f03(int a, int b) { return a + b + 3; }
+int f04(int b, int a, int c) { return a + b + c + 4; }
+int f05(int a, int c, int b) { return a + b + c + 5; }
+int f06(int c, int b, int a) { return a + b + c + 6; }
+int f07(int b, int c, int a) { return a + b + c + 7; }
+int f08(int c, int a, int b) { return a + b + c + 8; }
+int f09(int d, int a, int b, int c) { return a + b + c + d + 9; }
+int f10(int a, int d, int b, int c) { return a + b + c + d + 10; }
+int f11(int a, int b, int d, int c) { return a + b + c + d + 11; }
+int f12(int a, int b, int c, int d) { return a + b + c + d + 12; }
 
 struct S {
   int (*f01)(int b, int c);
