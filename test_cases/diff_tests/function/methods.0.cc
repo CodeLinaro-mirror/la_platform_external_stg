826da9f4d6a0c9c999a81d15d4335349a0945d28
Giuliano Procida <gprocida@google.com>
2023-11-13 18:13:38 +0000

    test cases: change "par" to "parameter" in function/methods test case
    This was an unhelpful abbreviation.
    
    PiperOrigin-RevId: 582004572
    Change-Id: I53dd779b2338bf32cbe475d4892a28e866b5c101

--- test_cases/diff_tests/function/methods.0.cc
+++ test_cases/diff_tests/function/methods.0.cc
@@ -3,20 +3,20 @@ struct Func {
   int change_return_type();
 
   // add or remove
-  int add_par();
-  int remove_par(int remove);
-  int change_par_type(int par);
+  int add_parameter();
+  int remove_parameter(int remove);
+  int change_parameter_type(int parameter);
   int rename_old();
 
   // no diff
-  int change_par_name(int remove);
+  int change_parameter_name(int remove);
 
   int x;
 } var;
 
 int Func::change_return_type() { return 0; }
-int Func::add_par() { return 0; }
-int Func::remove_par(int remove) { return remove; }
-int Func::change_par_type(int par) { return par; }
+int Func::add_parameter() { return 0; }
+int Func::remove_parameter(int remove) { return remove; }
+int Func::change_parameter_type(int parameter) { return parameter; }
 int Func::rename_old() { return 0; }
-int Func::change_par_name(int remove) { return remove; }
+int Func::change_parameter_name(int remove) { return remove; }
--- test_cases/diff_tests/function/methods.1.cc
+++ test_cases/diff_tests/function/methods.1.cc
@@ -3,20 +3,20 @@ struct Func {
   long change_return_type();
 
   // add or remove
-  int add_par(int add);
-  int remove_par();
-  int change_par_type(long par);
+  int add_parameter(int add);
+  int remove_parameter();
+  int change_parameter_type(long parameter);
   int rename_new();
 
   // no diff
-  int change_par_name(int add);
+  int change_parameter_name(int add);
 
   long x;
 } var;
 
 long Func::change_return_type() { return 0; }
-int Func::add_par(int add) { return add; }
-int Func::remove_par() { return 0; }
-int Func::change_par_type(long par) { return par; }
+int Func::add_parameter(int add) { return add; }
+int Func::remove_parameter() { return 0; }
+int Func::change_parameter_type(long parameter) { return parameter; }
 int Func::rename_new() { return 0; }
-int Func::change_par_name(int add) { return add; }
+int Func::change_parameter_name(int add) { return add; }
