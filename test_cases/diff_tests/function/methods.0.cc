57f3d70d719c7cd6c1edb2c8d8356d227b203282
Giuliano Procida <gprocida@google.com>
2023-11-15 12:08:51 +0000

    test cases: simplify function/methods diff test
    The functions don't need to do anything with their parameters.
    
    PiperOrigin-RevId: 582619641
    Change-Id: I265f6bf9d163ed2726c30e1344f3067f69b8c92d

--- test_cases/diff_tests/function/methods.0.cc
+++ test_cases/diff_tests/function/methods.0.cc
@@ -4,19 +4,19 @@ struct Func {
 
   // add or remove
   int add_parameter();
-  int remove_parameter(int remove);
-  int change_parameter_type(int parameter);
+  int remove_parameter(int);
+  int change_parameter_type(int);
   int rename_old();
 
   // no diff
-  int change_parameter_name(int remove);
+  int change_parameter_name(int);
 
   int x;
 } var;
 
 int Func::change_return_type() { return 0; }
 int Func::add_parameter() { return 0; }
-int Func::remove_parameter(int remove) { return remove; }
-int Func::change_parameter_type(int parameter) { return parameter; }
+int Func::remove_parameter(int) { return 0; }
+int Func::change_parameter_type(int) { return 0; }
 int Func::rename_old() { return 0; }
-int Func::change_parameter_name(int remove) { return remove; }
+int Func::change_parameter_name(int) { return 0; }
--- test_cases/diff_tests/function/methods.1.cc
+++ test_cases/diff_tests/function/methods.1.cc
@@ -3,20 +3,20 @@ struct Func {
   long change_return_type();
 
   // add or remove
-  int add_parameter(int add);
+  int add_parameter(int);
   int remove_parameter();
-  int change_parameter_type(long parameter);
+  int change_parameter_type(long);
   int rename_new();
 
   // no diff
-  int change_parameter_name(int add);
+  int change_parameter_name(int);
 
   long x;
 } var;
 
 long Func::change_return_type() { return 0; }
-int Func::add_parameter(int add) { return add; }
+int Func::add_parameter(int) { return 0; }
 int Func::remove_parameter() { return 0; }
-int Func::change_parameter_type(long parameter) { return parameter; }
+int Func::change_parameter_type(long) { return 0; }
 int Func::rename_new() { return 0; }
-int Func::change_parameter_name(int add) { return add; }
+int Func::change_parameter_name(int) { return 0; }
