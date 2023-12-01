12254b5683084ee4dc448bc1634504cfffe7f323
Giuliano Procida <gprocida@google.com>
2023-12-01 14:40:16 +0000

    type roots: ignore typedefs that would have been filtered out
    When file-based type definition filtering (`--files`) is active, any
    type not matching the filter will not be considered a type root
    (`--types`). Previous to this change, such `typedef`s were still
    considered type roots.
    
    `typedef`, as a C and C++ language feature, does not permit forward
    declaration and STG does not model `typedef`s without a definition. STG
    only considers named types with definitions as type roots. Logically, if
    a `typedef` would have lost its definition, it should not be considered
    a type root.
    
    PiperOrigin-RevId: 586994877
    Change-Id: I69c9a4069b7dca2d1e348e6690566ca843f4aba6

--- dwarf_processor.cc
+++ dwarf_processor.cc
@@ -424,6 +424,11 @@ class Processor {
     const std::string type_name = scope_ + GetName(entry);
     auto referred_type_id = GetIdForReferredType(MaybeGetReferredType(entry));
     const Id id = AddProcessedNode<Typedef>(entry, type_name, referred_type_id);
+    if (!ShouldKeepDefinition(entry, type_name)) {
+      // We always model (and keep) typedef definitions. But we should exclude
+      // filtered out types from being type roots.
+      return;
+    }
     AddNamedTypeNode(id);
   }
 
