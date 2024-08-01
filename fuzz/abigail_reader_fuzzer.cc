1672e5dcf2fcba003ee5d9759223b43fcfb44755
Giuliano Procida <gprocida@google.com>
2024-08-01 11:32:44 +0100

    Abigail reader fuzzer: eschew abbreviated "doc"
    PiperOrigin-RevId: 658340831
    Change-Id: Iae5964cb515227a5f1f00489d053bc11752580aa

--- fuzz/abigail_reader_fuzzer.cc
+++ fuzz/abigail_reader_fuzzer.cc
@@ -31,17 +31,17 @@ extern "C" int LLVMFuzzerTestOneInput(char* data, size_t size) {
   xmlParserCtxtPtr ctxt = xmlNewParserCtxt();
   // Suppress libxml error messages.
   xmlSetGenericErrorFunc(ctxt, (xmlGenericErrorFunc) DoNothing);
-  xmlDocPtr doc = xmlCtxtReadMemory(
+  xmlDocPtr document = xmlCtxtReadMemory(
       ctxt, data, size, nullptr, nullptr,
       XML_PARSE_NOERROR | XML_PARSE_NONET | XML_PARSE_NOWARNING);
   xmlFreeParserCtxt(ctxt);
 
-  // Bail out if the doc XML is invalid.
-  if (!doc) {
+  // Bail out if the document XML is invalid.
+  if (document == nullptr) {
     return 0;
   }
 
-  xmlNodePtr root = xmlDocGetRootElement(doc);
+  xmlNodePtr root = xmlDocGetRootElement(document);
   if (root) {
     try {
       stg::Graph graph;
@@ -51,7 +51,7 @@ extern "C" int LLVMFuzzerTestOneInput(char* data, size_t size) {
     }
   }
 
-  xmlFreeDoc(doc);
+  xmlFreeDoc(document);
 
   return 0;
 }
