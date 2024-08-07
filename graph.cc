81a957108a778e5d3b3cbee0169458c134b26b98
Giuliano Procida <gprocida@google.com>
2024-08-07 18:41:55 +0100

    model: add ELF NOTYPE symbol type
    This will be necessary to handle undefined symbols in `.symtab` (as
    found in `vmlinux`, `*.ko` as well as `*.o` object files). It is also
    necessary to handle certain x86_64 Linux kernel symbols.
    
    PiperOrigin-RevId: 660449037
    Change-Id: I62ee76ff8aa0773370d40dfe691cf692c4ff8bbc

--- graph.cc
+++ graph.cc
@@ -82,6 +82,8 @@ std::ostream& operator<<(std::ostream& os, Qualifier qualifier) {
 
 std::ostream& operator<<(std::ostream& os, ElfSymbol::SymbolType type) {
   switch (type) {
+    case ElfSymbol::SymbolType::NOTYPE:
+      return os << "no-type";
     case ElfSymbol::SymbolType::OBJECT:
       return os << "variable";
     case ElfSymbol::SymbolType::FUNCTION:
