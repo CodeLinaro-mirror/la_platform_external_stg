949516b9a7ce39c6a95fde5890728a0b58d6b7a9
Giuliano Procida <gprocida@google.com>
2024-10-09 11:00:22 +0100

    scope: add a unit test
    This ensures the correct stack-like behaviour.
    
    PiperOrigin-RevId: 683961676
    Change-Id: Ide90dc43c1e46f5cb0abe9500e203ab273c59ca3

---
+++ scope_test.cc
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// -*- mode: C++ -*-
+//
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions (the
+// "License"); you may not use this file except in compliance with the
+// License.  You may obtain a copy of the License at
+//
+//     https://llvm.org/LICENSE.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Giuliano Procida
+
+#include "scope.h"
+
+#include <string>
+
+#include <catch2/catch.hpp>
+
+namespace Test {
+
+using stg::Scope;
+using stg::PushScopeName;
+
+TEST_CASE("scope") {
+  Scope scope;
+  CHECK(scope.empty());
+  {
+    const PushScopeName p1(scope, "1", "A");
+    CHECK(!scope.empty());
+    {
+      const PushScopeName p2 (scope, "2", std::string());
+      CHECK(!scope.empty());
+      {
+        const PushScopeName p3(scope, "3", "B");
+        CHECK(!scope.empty());
+      }
+      CHECK(!scope.empty());
+    }
+    CHECK(!scope.empty());
+  }
+  CHECK(scope.empty());
+}
+
+}  // namespace Test
