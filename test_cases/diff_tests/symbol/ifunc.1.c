671f0da7371d462c41c6efb0b332a7b333ad4e9a
Giuliano Procida <gprocida@google.com>
2026-06-10 09:51:14 +0100

    test cases: tweak IFUNC test
    * use `void` for empty parameter lists
    * make the return type `int` instead of `void`
    * add the missing parameter to the added function
    
    PiperOrigin-RevId: 929688979
    Change-Id: Ib93d08205f72c5159fbb9c5f1360983b24aef6a2

--- test_cases/diff_tests/symbol/ifunc.1.c
+++ test_cases/diff_tests/symbol/ifunc.1.c
@@ -1,13 +1,13 @@
-static void my_func(int new_arg) {  // new argument!
-  (void)new_arg;
+static int my_func(int new_arg) {
+  return new_arg;
 }
 
 __attribute__((__used__))
-static void (*resolve_func(void))(int) {
+static int (*resolve_func(void))(int) {
   return my_func;
 }
 
 // TODO: Add support for tracking type information
-void func_changed(int new_arg) __attribute__((ifunc("resolve_func")));
+int func_changed(int new_arg) __attribute__((ifunc("resolve_func")));
 
-void func_added() __attribute__((ifunc("resolve_func")));
+int func_added(int new_arg) __attribute__((ifunc("resolve_func")));
