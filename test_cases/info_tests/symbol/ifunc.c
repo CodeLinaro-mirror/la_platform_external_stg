ef592028bb5d5e8ef98a2cc75478781f4ab664b3
Giuliano Procida <gprocida@google.com>
2026-06-12 10:42:02 +0100

    test cases: info tests: tweak GNU IFUNC test
    Make C parameter lists `void` instead of empty.
    
    PiperOrigin-RevId: 931035396
    Change-Id: I4afe6fabcbf490677eb3f9e677f6e8497aae3b59

--- test_cases/info_tests/symbol/ifunc.c
+++ test_cases/info_tests/symbol/ifunc.c
@@ -1,8 +1,8 @@
-static void my_func() {}
+static void my_func(void) {}
 
 __attribute__((__used__))
 static void (*resolve_func(void))(void) {
   return my_func;
 }
 
-void func() __attribute__((ifunc("resolve_func")));
+void func(void) __attribute__((ifunc("resolve_func")));
